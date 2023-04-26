9b9b8591c30371ee915a734b14abb4c6bcfb5c67
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-26 10:11:34 +0800

    Pseudocode

---
+++ even_double_odd_triple.py
@@ -0,0 +1,17 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# This program identifies the even/odd numbers from a text file,
+# will get the square of each even number,
+# will get the cube of each odd number,
+# and will append the numbers in their respective text files.
+
+# open integer.txt, open double.txt, open triple.txt
+#for each line in nembers.txt
+    #convert each number in the line into integers
+    #check each integers
+    #if even
+        # get the square of the integer
+        # append to double.txt
+    #if odd
+        # get the cube of the integer
+        # append to triple.txt
