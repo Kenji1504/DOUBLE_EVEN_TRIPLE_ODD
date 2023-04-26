75c534e905147327638a364240a87401a03dbd18
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-26 10:33:48 +0800

    adding the text files

--- even_double_odd_triple.py
+++ even_double_odd_triple.py
@@ -6,12 +6,15 @@
 # and will append the numbers in their respective text files.
 
 # open integer.txt, open double.txt, open triple.txt
-#for each line in nembers.txt
-    #convert each number in the line into integers
-    #check each integers
-    #if even
-        # get the square of the integer
-        # append to double.txt
-    #if odd
-        # get the cube of the integer
-        # append to triple.txt
+with open ("integer.txt", "r") as integer_file, open("double.txt", "a") as double_file, open("triple.txt", "a") as triple_file: 
+    #for each line in nembers.txt
+    for line in integer_file:
+        print(line.strip())
+        #convert each number in the line into integers
+        #check each integers
+        #if even
+            # get the square of the integer
+            # append to double.txt
+        #if odd
+            # get the cube of the integer
+            # append to triple.txt
